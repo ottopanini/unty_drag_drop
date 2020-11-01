2de2cc711a47c5d036aea1eec37109f02346d7f4
cpress <cpress@it4ipm.de>
2020-11-01 15:25:52 +0100

    Interacting with DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,36 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-        console.log(this.templateElement);
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
-        console.log(this.hostElement);
         const importedNode = document.importNode(this.templateElement.content, true);
-        console.log(importedNode);
         this.element = importedNode.firstElementChild as HTMLFormElement;
-        console.log(this.element);
         this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attach();
-        console.log(this.hostElement);
     }
+
+    private submitHandler(event: Event) {
+        event.preventDefault(); //would trigger htttp event
+
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+
+    }
+
 
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
