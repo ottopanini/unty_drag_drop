be37f8d9f3cfa11a9747efa77599d6a17bc4929b
cpress <cpress@it4ipm.de>
2020-11-01 15:13:41 +0100

    DOM Element Selection & OOP

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        console.log(this.templateElement);
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        console.log(this.hostElement);
+        const importedNode = document.importNode(this.templateElement.content, true);
+        console.log(importedNode);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        console.log(this.element);
+        this.element.id = 'user-input';
+        this.attach();
+        console.log(this.hostElement);
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInput = new ProjectInput();
